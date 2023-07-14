b48c02f0a5aec6c3fd2dc2d667ad3b74f48e5fbd
Prostomol4i <prosto.mol4i.okk@gmail.com>
2023-07-14 13:37:36 +0600

    Сверстать страницу сканирования QR-кода

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:nft_brand_scanner/src/screens/auth/auth_screen.dart';
+// import 'package:nft_brand_scanner/src/screens/auth/auth_screen.dart';
+import 'package:nft_brand_scanner/src/screens/qr_scanner_screen/qr_scanner_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const CupertinoApp(
-      home: AuthScreen(),
+      home: QRScannerScreen(),
     );
   }
 }
